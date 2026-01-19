30715aa53f0111a9032c814a9300eae336068e4e
Mubulik <93089143+Mubulik@users.noreply.github.com>
2026-01-19 20:24:32 +0100

    Update vpn_install.sh
    - Ajout d'une vérification d'existance de NetworkManager
    - Sécurisation des commandes en utilisant les chemins absolus (recommendation de Mr. L. Écotière)
    - Redirection de l'output des commandes update et install des dépendances (également par reco de Mr. L. Écotière)
    - Plus d'output textuel pour l'utilisateur, suivi d'avancement de la configuration

--- vpn_install.sh
+++ vpn_install.sh
@@ -2,47 +2,54 @@
 # Script créé par Lilian Lambert, le 19/01/2026, élève de l'UMLP, en BUT1 R&T
 # Fonctionne sous : Debian13
 
+# Faire un système de log
+
+# Vérification de la présence ou non de NetworkManager
+if [ ! -f /bin/nmcli ]; then
+    echo "L'hôte ne dispose pas de NetworkManager, obligatoire pour la gestion de connexion VPN via ce script."
+    exit 1
+fi
+
 # Vérification de l'existence ou non d'une connexion VPN avec le nom donné par l'utilisateur
 vpnExists=1
 while [ $vpnExists -eq 1 ]; do
-    echo -n "Entrez le nom de la connexion VPN : " ; read vpnName
+    /bin/echo -n "Entrez le nom de la connexion VPN : " ; read vpnName
     if [ -e "/etc/NetworkManager/system-connections/$vpnName.nmconnection" ]; then
-        echo "La connexion VPN $vpnName existe déjà. Veuillez choisir un autre nom ou la supprimer."
+        /bin/echo "La connexion VPN $vpnName existe déjà. Veuillez choisir un autre nom ou la supprimer."
         exit 1
     fi
     vpnExists=0
 done
 
 # Init des variables
-echo -n "Entrez votre nom d'utilisateur dans le domaine (sans le @ufc) : " ; read user
+/bin/echo -n "Entrez votre nom d'utilisateur dans le domaine (sans le @ufc) : " ; read user
 address="vpn20-2.univ-fcomte.fr"
 domainName="ufc"
 
 # Ajout du nom de domaine dans le nom d'utilisateur
-user=$(echo "$user@$domainName")
+user=$(/bin/echo "$user@$domainName")
 
 
 # Lancement de l'installation
-echo -n "Appuyez sur une touche pour démarrer la configuration du VPN..."
+/bin/echo -n "Appuyez sur une touche pour démarrer la configuration du VPN..."
 read vartrash
 
 # Update des dépôts et installation des prérequis
-sudo apt-get update
-sudo apt install -y network-manager-strongswan libstrongswan-extra-plugins libcharon-extra-plugins
+echo "Mise à jour et installation des prérequis..."
+$(/bin/sudo /bin/apt-get update) 2>/dev/null
+$(/bin/sudo /bin/apt-get install -y network-manager-strongswan libstrongswan-extra-plugins libcharon-extra-plugins) 2>/dev/null
 
-
-
-# Création de la connexion vpn + récupération de l'UUID LETS GOOOO
-uuid=$(sudo nmcli connection add con-name "$vpnName" type vpn vpn-type strongswan | awk -F'[()]' '{print $2}')
-echo "UUID enregistré : $uuid"
+# Création de la connexion vpn + récupération de l'UUID
+/bin/echo "Configuration de la connexion VPN..."
+uuid=$(/bin/sudo /bin/nmcli connection add con-name "$vpnName" type vpn vpn-type strongswan | /bin/awk -F'[()]' '{print $2}')
 
 # Suppression du fichier créé dans /etc/NetworkManager/system-connections
-sudo rm /etc/NetworkManager/system-connections/"$vpnName".nmconnection
+/bin/sudo /bin/rm /etc/NetworkManager/system-connections/"$vpnName".nmconnection
 
 # Création du fichier de conf de la connexion
-sudo touch /etc/NetworkManager/system-connections/"$vpnName".nmconnection
+/bin/sudo /bin/touch /etc/NetworkManager/system-connections/"$vpnName".nmconnection
 
-sudo echo "[connection]
+/bin/sudo /bin/echo "[connection]
 id=$vpnName
 uuid=$uuid
 type=vpn
@@ -68,7 +75,11 @@ method=auto
 [proxy]"> /etc/NetworkManager/system-connections/"$vpnName".nmconnection
 
 # Attributions des bons droits
-sudo chmod 600 /etc/NetworkManager/system-connections/"$vpnName".nmconnection
+/bin/sudo /bin/chmod 600 /etc/NetworkManager/system-connections/"$vpnName".nmconnection
 
 # Reload des connexions NetworkManager
-sudo nmcli connection reload
+/bin/sudo /bin/nmcli connection reload
+
+/bin/echo "Configuration de la connexion VPN terminée.
+Lors de la connexion, le mot de passe demandé est celui de l'ENT de votre école.
+"
